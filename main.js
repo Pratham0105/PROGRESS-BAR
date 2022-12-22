f3a7d86f9e6c353c48c26d7a3cf1906325e6f6f5
Pratham Parashar <pratham.parashar143858@gmail.com>
2022-12-22 11:07:46 +0530

    Progress Bar
    n this project, the circle's stroke and the number of the percentage animate from 0 to a given value. To make this project [Circular Progress Bar], I have used pure HTML, CSS, and JavaScript code.

---
+++ main.js
@@ -0,0 +1,20 @@
+function progress_bar(){
+    let circularProgress = document.querySelector(".cerculer-progress"),
+    progressValue = document.querySelector(".progress-value");
+
+let progressStartValue = 0,    
+    progressEndValue = 90,    
+    speed = 100;
+    
+let progress = setInterval(() => {
+    progressStartValue++;
+
+    progressValue.textContent = `${progressStartValue}%`
+    circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #ededed 0deg)`
+
+    if(progressStartValue == progressEndValue){
+        clearInterval(progress);
+    }    
+}, speed);
+}
+
